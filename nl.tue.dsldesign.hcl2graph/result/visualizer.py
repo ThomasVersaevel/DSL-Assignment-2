42e506a29e583aa3f8fbd1f0e4e1bda258dc1959
El-Merr <49949306+El-Merr@users.noreply.github.com>
2022-06-15 15:41:32 +0200

    PUSH

--- nl.tue.dsldesign.hcl2graph/result/visualizer.py
+++ nl.tue.dsldesign.hcl2graph/result/visualizer.py
@@ -1,7 +1,18 @@
-import xml.etree.ElementTree
+import xml.etree.ElementTree as ET
 import graphviz
 
-tree = xml.etree.ElementTree.parse('TransformedHCL')
+import os
+# os.environ["PATH"] += os.pathsep +'C:/Users/Merel/anaconda3/Lib/site-packages/graphviz/'
+
+# print(os.getcwd())
+# filename = 'D:/GitHub/DSL-Assignment-2/nl.tue.dsldesign.hcl2graph/result/TransformedHCL'
+filename = 'TransformedHCL'
+
+if not os.path.exists(filename):
+    print('ERROR : This path does not exist !!')
+
+with open(filename, 'rb') as file:
+    tree = ET.parse(file)
 root = tree.getroot()
 
 structure_name = 'HCLgraph' # structure.findtext('./Name')
@@ -9,18 +20,37 @@ graph = graphviz.Digraph(format='svg',comment='structure_name')
 
 graphloc = '{http://dsldesign.tue.nl/graph}'
 
-# print(root[1])
-for node in root.findall(graphloc+'Graph'): # root: #.findall('./Graph'):
-    print(node)
+print(root[1])
+# for node in root.findall(graphloc+'Graph'): # root: #.findall('./Graph'):
+#     print(node)
+
+# class Node:
+#   def __init__(self, label, nid):
+#     self.label = label
+#     self.nid = "//@nodes.ID={}".format(nid)
+#     #self.shape = shape
+
+nodelist = []
+i = 0
 
 # read all the structures
-for node in root.findall(graphloc+'Node'): # root: #.findall('./Graph'):
+for node in root.findall('nodes'): # root: #.findall('./Graph'):
+    label = node.attrib['label']
+    nodeId = "//@nodes.ID={}".format(i)
+    # n = Node(node.attrib['label'], i)
+    # nodelist.append(n)
+    
+    graph.node(nodeId, label)
+    i += 1
+    
     print(node)
-    for a in node:
-        print(a)
-    labelid = node.attrib['label']
-    print(labelid)
-    graph.node(labelid, labelid)
+    
+    for edge in node:
+        target = edge.attrib['target']
+        print(edge)
+        print(target)
+        graph.edge(nodeId, target)
+    
     
     # read the nodes of the structure
     # for node in structure.findall('.//Nodes/Node'):
@@ -39,4 +69,4 @@ for node in root.findall(graphloc+'Node'): # root: #.findall('./Graph'):
     #     if direction=='Double':
     #         graph.edge(id_2, id_1, label=description)
 
-# graph.render(filename=structure_name)
+graph.render(filename=structure_name)
